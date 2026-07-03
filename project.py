c3d6319c24f14f7b61bfbe6120075355e2e28bb0
Weronika Łabęcka <weronika@MacBook-Pro-Weronika.local>
2026-07-03 22:07:52 +0200

    Task3: zapis json

---
+++ project.py
@@ -0,0 +1,16 @@
+import sys
+import json
+import yaml
+import xml.etree.ElementTree as ET
+import os
+
+def get_extension(path):
+    return os.path.splitext(path)[1].lower()
+
+def read_json(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+    
+def write_json(data, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
